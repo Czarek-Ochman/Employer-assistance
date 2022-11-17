65bef425731b36c2164b84ceb06eb1cd821428de
Czarek-Ochman <czarek.ochman@gmail.com>
2022-11-17 12:31:55 +0100

    initial commit

---
+++ src/test/java/pl/employer/assistance/AssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.employer.assistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
